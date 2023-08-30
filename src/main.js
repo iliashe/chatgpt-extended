af6da39f3853acac79a0f9fcd87762abeaf7c47e
iliashe <schestakov.ilia@gmail.com>
2023-08-30 22:14:34 +0200

    opanai api

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+import axios from "axios";
+const options = {
+    method: 'GET',
+    url: "http://localhost:5000",
+  }
+  axios.request(options)
+  .then(res => console.log(res))
+
+
 const app = createApp(App)
 
 app.use(createPinia())
