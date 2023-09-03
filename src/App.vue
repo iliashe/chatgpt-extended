e157bef9e41e05d29ddd2b37ee60436f9303d5e6
iliashe <schestakov.ilia@gmail.com>
2023-09-03 20:30:54 +0200

    test auth

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
-<script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
-
-import Header from './components/Header.vue'
-import Sidebar from './components/Sidebar.vue'
-import Chat from './components/Chat.vue'
-import Input from './components/Input.vue'
-</script>
-
 <template>
-  <aside class="w-1/6">
-    <Sidebar />
-  </aside>
+  <div class="h-full w-full flex" v-if="user.isLogged">
+    <aside class="h-full w-1/6">
+      <Sidebar />
+    </aside>
+    
+    <div class="h-full w-full flex flex-col">
+      <header class="w-full">
+        <Header />
+      </header>
   
-  <div class="w-full flex flex-col">
-    <header class="w-full">
-      <Header />
-    </header>
-
-    <div class="h-full w-full">
-      <Chat />
+      <div class="h-full w-full">
+        <Chat />
+      </div>
+  
+      <div class="min-h-1/3 px-2 py-4">
+        <Input />
+      </div>
     </div>
+  </div>
 
-    <div class="min-h-1/3 px-2 py-4">
-      <Input />
-    </div>
+  <div class="" v-else>
+    <Form />
   </div>
   <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
@@ -38,3 +34,18 @@ import Input from './components/Input.vue'
     <TheWelcome />
   </main> -->
 </template>
+
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+
+import Header from './components/Header.vue'
+import Sidebar from './components/Sidebar.vue'
+import Chat from './components/Chat.vue'
+import Input from './components/Input.vue'
+import Form from './components/Form.vue'
+
+import { useUserStore } from '@/stores/user'
+
+const user = useUserStore()
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,52 @@
+<template>
+  <form class="border-2 border-white px-4 py-8 flex flex-col gap-4">
+    <div class="flex flex-col">
+      <label for="username">Username</label>
+      <input v-model="username" class="text-black" type="text" name="username" placeholder="Hulk" />
+    </div>
+
+    <div class="flex flex-col">
+      <label for="email">Email<sup>*</sup></label>
+      <input v-model="email" class="text-black" type="email" name="email" placeholder="example@email.com" required />
+    </div>
+
+    <div class="flex flex-col">
+      <label for="password">Password<sup>*</sup></label>
+      <input v-model="password" class="text-black" type="password" name="password" placeholder="qwerty123" required />
+    </div>
+  </form>
+
+  <button @click="submitForm()" type="submit" class="p-4 border-2 border-white">
+    Submit
+  </button>
+</template>
+
+<script setup>
+import axios from "axios"
+import { ref } from "vue"
+import { useUserStore } from "@/stores/user"
+
+const user = useUserStore()
+
+const username = ref("")
+const email = ref("")
+const password = ref("")
+
+function submitForm() {
+  if (email && password) {
+    axios
+      .post(import.meta.env.VITE_API_URL + '/user/login', {
+        email: email.value,
+        password: password.value
+      })
+      .then(res => {
+        if (res.data) {
+          user.isLogged = true
+        } else {
+          console.log(res)
+        }
+      })
+      .catch(err => console.log(err))
+  }
+}
+</script>
